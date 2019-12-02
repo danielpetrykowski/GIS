d195e7ea6f26a5312ed858dffc436edfebf973b3
Grzegorz Wojciechowski <gwojciech@newterm.pl>
2019-12-02 17:48:28 +0100

    Skeleton of C++ Ford-Fulkerson

---
+++ CPP/ford_fulkerson.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <limits.h>
+#include <string.h>
+#include <queue>
+
+
+int loadGraph()
+{
+	std::cout << "Graph loaded" << std::endl;
+}
+
+bool bfs(int graph[], int s, int t, int parent[])
+{
+	return true;
+}
+
+int fordFulkerson(int graph, int s, int t)
+{
+	return 5;
+}
+
+int main()
+{
+	loadGraph();
+
+	std::cout << "The maximum possible flow is " << fordFulkerson(5, 0, 5) << std::endl;
+
+	return 0;
+}
+
