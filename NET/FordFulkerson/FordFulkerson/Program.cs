3419373210f4facc99c7ac11d7fb0011bc16373e
Daniel Petrykowski <daniel.petrykowski7@gmail.com>
2020-01-05 22:53:09 +0100

    Zegar - implementacja C#

--- NET/FordFulkerson/FordFulkerson/Program.cs
+++ NET/FordFulkerson/FordFulkerson/Program.cs
@@ -1,17 +1,32 @@
 ﻿using FordFulkerson.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace FordFulkerson
 {
     class Program
     {
+        private static long frequency = Stopwatch.Frequency;
+
         static void Main(string[] args)
         {
+            Stopwatch stopWatch = new Stopwatch();
+            
             List<string> fileLines = TextFile.ReadFileLines();
             FlowGraph flowGraph = new FlowGraph(fileLines);
+
+
+            stopWatch.Start();
             var maxFlow = flowGraph.FordFulkerson();
+            stopWatch.Stop();
+
+
+            TimeSpan ts = stopWatch.Elapsed;
+            var elapsedTime = (decimal) ts.Ticks / frequency;
+
             Console.WriteLine(maxFlow);
+            Console.WriteLine($"RunTime " + elapsedTime.ToString("F10") + "s");
         }
     }
 }
