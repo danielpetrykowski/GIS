979fbb3aa5b586675ceb304b066878c4043488da
Grzegorz Wojciechowski <gwojciech@newterm.pl>
2020-01-09 19:01:52 +0100

    Working code for both linux and windows

--- CPP/ford_fulkerson.cpp
+++ CPP/ford_fulkerson.cpp
@@ -3,30 +3,35 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+
+#ifdef _WIN32
 #include <sys/types.h>
 #include <dirent.h>
-
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
 
 int main()
 {
 #ifdef _WIN32
-	// Tu sciezka windows way
 	std::string path = "./test";
-#else
-	std::string path = "./graphs";
-#endif
     DIR* dirp = opendir(path.c_str());
     struct dirent * dp;
 	while ((dp = readdir(dirp)) != NULL) {
-		//std::cout << entry.path() << std::endl;
 		std::string fileName = dp->d_name;
 		if(fileName == "." || fileName== "..") continue;
 		std::ifstream infile(path + "/" + fileName);
+#else
+	std::string path = "./graphs";
+	for (const auto& entry : fs::directory_iterator(path)) {
+		std::ifstream infile(entry.path());
+#endif
 		std::string line;
 		std::vector<std::string> lines;
 		while (std::getline(infile, line)) {
 			lines.push_back(line);
-		}		
+		}
 
 		if (lines.size() == 0) {
 			std::cout << "No file given" << std::endl;
