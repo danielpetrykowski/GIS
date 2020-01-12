7f11d0a570cc1d904140a88d694bab63dfd37938
Grzegorz Wojciechowski <gwojciech@newterm.pl>
2020-01-12 11:47:46 +0100

    Bonusowy test do sprawka, troche wnioskow

--- NetworkGenerator/NetworkGenerator/Program.cs
+++ NetworkGenerator/NetworkGenerator/Program.cs
@@ -6,8 +6,8 @@ namespace NetworkGenerator
     class Program
     {
         private const int VERTICAL = 2000;
-        private const int AVG_VERTICAL_DEGREE = 2000;
-        private const int MAX_VALUE = 100;
+        private const int AVG_VERTICAL_DEGREE = 1000;
+        private const int MAX_VALUE = 1000000;
         private const int MIN_VALUE = 0;
 
         static void Main(string[] args)
